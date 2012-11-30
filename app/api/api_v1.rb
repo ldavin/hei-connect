0d6c42dfdd94f242b1848f6a6609666dd2695210
Louis Davin <louis.developpement@gmail.com>
2012-11-30 19:17:42 +0100

    Draft API access for the schedule action

--- app/api/api_v1.rb
+++ app/api/api_v1.rb
@@ -4,9 +4,19 @@ module API
     version 'v1', :using => :path
 
     resource :test do
-      post :login do
-        client = Konosys::Login.new(params[:username], params[:password])
-        client.proceed
+      desc 'Test fetching the courses of the current week'
+      post :schedule do
+        schedule = Konosys::Actions::Schedule.new(params[:username], params[:password])
+
+        begin
+          week = schedule.get_current_week
+        rescue Konosys::Exceptions::LoginError
+          error! 'Login failed', 401
+        end
+
+        schedule.finish
+
+        present week, with: Konosys::Models::Week::Entity
       end
     end
 
