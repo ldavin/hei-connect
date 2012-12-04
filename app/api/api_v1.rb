2647ab27376b4ef24c254b22f8fd224c80b9c48f
Louis Davin <louis.developpement@gmail.com>
2012-12-04 09:39:37 +0100

    Add logging

--- app/api/api_v1.rb
+++ app/api/api_v1.rb
@@ -3,6 +3,20 @@ module API
 
     version 'v1', :using => :path
 
+    helpers do
+      def logger
+        API.logger
+      end
+    end
+
+    before do
+      logger.info {
+        "#{params[:route_info].route_method} #{params[:route_info].route_version}/" +
+            "#{params[:route_info].route_path.split('/')[2..-1].join('/').gsub(/\(.*\)/, '')}" +
+            " for #{params[:username]}"
+      }
+    end
+
     resource :schedule do
       desc 'Fetch the courses of the current and next week'
       get '/' do
@@ -11,7 +25,8 @@ module API
         begin
           weeks = schedule.fetch_current_and_next_week
         rescue Konosys::Exceptions::LoginError
-          error! 'Login failed', 401
+          logger.error "Login failed for #{params[:username]}"
+          error!({error: 'Login failed'}, 401)
         end
 
         schedule.finish
