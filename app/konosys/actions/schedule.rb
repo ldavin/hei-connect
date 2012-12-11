8aa9effe385c65e6b0c5d2846e0b77aa94c9bf96
Louis Davin <louis.developpement@gmail.com>
2012-12-11 22:46:30 +0100

    Try to fix encoding problems with force_encoding

--- app/konosys/actions/schedule.rb
+++ app/konosys/actions/schedule.rb
@@ -8,9 +8,9 @@ module Konosys
         login
 
         @browser.goto(SCHEDULE_URL)
-        current_week_source = @browser.xml.encode('utf-8')
+        current_week_source = @browser.xml.force_encoding('utf-8')
         @browser.button(:id, 'semsuiv').click # Move to next week
-        next_week_source = @browser.xml.encode('utf-8')
+        next_week_source = @browser.xml.force_encoding('utf-8')
 
         weeks = Array.new
         weeks.push Models::Week.new(current_week_source)
