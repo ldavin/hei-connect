184c145813cd47e54924dd7af32a0df82490763c
Louis Davin <louis.developpement@gmail.com>
2013-03-13 13:39:28 -0400

    [Fixes #46128275] Rationalize the routes and actions

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ HeiConnect::Application.routes.draw do
   end
 
   api version: 2, module: 'V2' do
-    get 'absences', :to => 'absences#show', :as => :v2_absences
-    get 'grades', :to => 'grades#show', :as => :v2_grades
-    get 'grades_detailed', :to => 'grades#show_detailed', :as => :v2_grades_detailed
-    get 'schedule', :to => 'schedules#show', :as => :v2_schedule
-    get 'sessions/absences', :to => 'sessions#absences', :as => :v2_absences_sessions
-    get 'sessions/grades', :to => 'sessions#grades', :as => :v2_grades_sessions
+    get 'absences/:token', :to => 'absences#show', :as => :v2_absences
+    get 'grades/:token', :to => 'grades#show', :as => :v2_grades
+    get 'grades/:token/detailed', :to => 'grades#show_detailed', :as => :v2_grades_detailed
+    get 'schedules/:token', :to => 'schedules#show', :as => :v2_schedule
+    get 'sessions/:token/absences', :to => 'sessions#absences', :as => :v2_absences_sessions
+    get 'sessions/:token/grades', :to => 'sessions#grades', :as => :v2_grades_sessions
     post 'users', :to => 'users#create', :as => :v2_users
     get 'users', :to => 'users#show'
   end
