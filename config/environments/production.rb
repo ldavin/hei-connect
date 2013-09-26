eab8e14c0b2de92b66da8efdd9d3d24ba9a6f3d9
Louis Davin <ldavin@octo.com>
2013-09-26 11:31:15 +0200

    Add loggly properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,7 @@ HeiConnect::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Send logs to loggly
+  config.logger = Logglier.new(ENV['LOGGLY_RAILS'], :threaded => true)
 end
