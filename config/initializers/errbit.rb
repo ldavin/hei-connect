3b1213aee90ab5fc29f4537abc39582fa5c10673
Louis Davin <ldavin@octo.com>
2013-09-26 18:18:10 +0200

    Replace airbrake by exceptional notifier

--- config/initializers/errbit.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-  config.api_key = ENV['ERRBIT_API_KEY']
-  config.host    = 'hei-connect-error.herokuapp.com'
-  config.port    = 80
-  config.secure  = config.port == 443
-end
